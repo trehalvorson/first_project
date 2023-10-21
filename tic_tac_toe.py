b3c6e7d84acce10c3656df5684a82969d7447ae4
Tre Halvorson <trehalvorson2@gmail.com>
2023-10-21 17:08:05 -0500

    Create tic_tac_toe.py

---
+++ tic_tac_toe.py
@@ -0,0 +1,25 @@
+row0 = [" ", " ", " "]
+row1 = [" ", " ", " "]
+row2 = [" ", " ", " "]
+board = [row0, row1, row2]
+
+def printBoard():
+ print("")
+ print(board[0][0], "|", board[0][1], "|", board[0][2])
+ print("--+---+--")
+ print(row1[0], "|", row1[1], "|", row1[2])
+ print("--+---+--")
+ print(row2[0], "|", row2[1], "|", row2[2])
+ print("")
+
+printBoard()
+
+pos = input("where? ")
+pos = int(pos)
+print(pos)
+
+row = int((pos - 1) / 3)
+column = ((pos - 1) % 3)
+print(row)
+print(column)
+print(board[row][column], row, column)
