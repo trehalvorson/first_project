6db0f68ada126cbfadcad55f0f0ca439968e5bcc
Tre Halvorson <trehalvorson2@gmail.com>
2023-10-21 19:18:15 -0500

    Replace different values based on turn

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,27 +5,41 @@ row2 = [" ", " ", " "]
 board = [row0, row1, row2]
 
 # Prints the board
-def printBoard():
+def printBoard(board):
  print("")
  print(board[0][0], "|", board[0][1], "|", board[0][2])
  print("--+---+--")
- print(row1[0], "|", row1[1], "|", row1[2])
+ print(board[1][0], "|", board[1][1], "|", board[1][2])
  print("--+---+--")
- print(row2[0], "|", row2[1], "|", row2[2])
+ print(board[2][0], "|", board[2][1], "|", board[2][2])
  print("")
 
-printBoard()
+printBoard(board)
+
+playerTurn = "X"
 
 # Repeats forever
 while True:
- pos = input("where? ")
+ pos = input(playerTurn + " is up, what square will you play? ")
  if pos == "stop":
+   print("")
+   print("")
+   print("")
+   print("")
+   print("You have ended the game. Final Board:")
+   printBoard(board)
    break # Stops the forever loop with the keyword
  else:
   pos = int(pos)
  
   row = int((pos - 1) / 3)
   column = ((pos - 1) % 3)
-  board[row][column] = "X"
+  board[row][column] = playerTurn
 
-  printBoard()
+  printBoard(board)
+
+  # Switches playerTurn between "X" and "O"
+  if playerTurn == "X":
+   playerTurn = "O"
+  else:
+   playerTurn = "X"
